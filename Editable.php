01d51a84377027cc27256101ddbf52d1015dbecc
Chillance <danielakesson@gmail.com>
2013-03-24 00:58:56 +0100

    Created Editable 'base' class which doesn't require models.

---
+++ Editable.php
@@ -0,0 +1,591 @@
+<?php
+/**
+ * Editable class file.
+ *
+ * @author Vitaliy Potapov <noginsk@rambler.ru>
+ * @link https://github.com/vitalets/x-editable-yii
+ * @copyright Copyright &copy; Vitaliy Potapov 2012
+ * @version 1.0.0
+*/
+
+/**
+* EditableField widget makes editable single attribute of model.
+*
+* @package widgets
+*/
+class Editable extends CWidget
+{
+    //note: only most usefull options are on first level of config.
+    /**
+     * @var id unique id
+    */
+    public $id = null;
+
+     /**
+    * @var string type of editable widget. Can be `text`, `textarea`, `select`, `date`, `checklist`, etc.
+    * @see x-editable
+    */
+    public $type = null;
+    /**
+    * @var string url to submit value. Can be string or array containing Yii route, e.g. `array('site/updateUser')`
+    * @see x-editable
+    */
+    public $url = null;
+    /**
+    * @var array additional params to send on server
+    * @see x-editable
+    */
+    public $params = null;
+    /**
+    * @var string css class of input. If `null` - default X-editable value is used: `input-medium`
+    * @see x-editable
+    */
+    public $inputclass = null;
+    /**
+    * @var string mode of input: `inline` | `popup`. If not set - default X-editable value is used: `popup`.
+    * @see x-editable
+    */
+    public $mode = null;
+    /**
+    * @var string text to be shown as element content
+    */
+    public $text = null;
+    /**
+    * @var mixed initial value. If not set - will be taken from text
+    * @see x-editable
+    */
+    public $value = null;
+    /**
+    * @var string placement of popup. Can be `left`, `top`, `right`, `bottom`. If `null` - default X-editable value is used: `top`
+    * @see x-editable
+    */
+    public $placement = null;
+
+    /**
+    * @var string text shown on empty field. If `null` - default X-editable value is used: `Empty`
+    * @see x-editable
+    */
+    public $emptytext = null;
+
+    /**
+    * @var boolean will editable be initially disabled. It means editable plugin will be applied to element,
+    * but you should call `.editable('enable')` method to activate it.
+    * To totally disable applying 'editable' to element use **apply** option.
+    * @see x-editable
+    */
+    public $disabled = false;
+
+    //list
+    /**
+    * @var mixed source data for **select**, **checklist**. Can be string (url) or array in format: 
+    * array( array("value" => 1, "text" => "abc"), ...)
+    * @package list
+    * @see x-editable
+    */
+    public $source = null;
+
+    //date
+    /**
+    * @var string format to send date on server. If `null` - default X-editable value is used: `yyyy-mm-dd`.
+    * @package date
+    * @see x-editable
+    */
+    public $format = null;
+    /**
+    * @var string format to display date in element. If `null` - equals to **format** option.
+    * @package date
+    * @see x-editable
+    */
+    public $viewformat = null;
+    /**
+    * @var string template for **combodate** input. For details see http://vitalets.github.com/x-editable/docs.html#combodate.
+    * @package combodate
+    * @see x-editable
+    */
+    public $template = null;        
+    /**
+    * @var array full config for **combodate** input. For details see http://vitalets.github.com/combodate/#docs
+    * @package combodate
+    * @see x-editable
+    */
+    public $combodate = null;    
+    /**
+    * @var string separator used to display tags.
+    * @package select2
+    * @see x-editable
+    */
+    public $viewseparator = null;        
+    /**
+    * @var array full config for **select2** input. For details see http://ivaynberg.github.com/select2
+    * @package select2
+    * @see x-editable
+    */
+    public $select2 = null;    
+    
+    //methods
+    /**
+    * A javascript function that will be invoked to validate value.
+    * Example:
+    * <pre>
+    * 'validate' => 'js: function(value) {
+    *     if($.trim(value) == "") return "This field is required";
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package callback
+    * @see x-editable
+    * @example
+    */
+    public $validate = null;
+    /**
+    * A javascript function that will be invoked to process successful server response.
+    * Example:
+    * <pre>
+    * 'success' => 'js: function(response, newValue) {
+    *     if(!response.success) return response.msg;
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package callback
+    * @see x-editable
+    */
+    public $success = null;
+    /**
+    * A javascript function that will be invoked to custom display value.
+    * Example:
+    * <pre>
+    * 'display' => 'js: function(value, sourceData) {
+    *      var escapedValue = $("&lt;div&gt;").text(value).html();
+    *      $(this).html("&lt;b&gt;"+escapedValue+"&lt;/b&gt;");
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package callback
+    * @see x-editable
+    */
+    public $display = null;
+
+
+    // --- X-editable events ---
+    /**
+    * A javascript function that will be invoked when editable element is initialized
+    * @var string
+    * @package event
+    * @see x-editable
+    */
+    public $onInit;
+    /**
+    * A javascript function that will be invoked when editable form is shown
+    * Example:
+    * <pre>
+    * 'onShown' => 'js: function() {
+    *     var $tip = $(this).data("editableContainer").tip();
+    *     $tip.find("input").val("overwriting value of input.");
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package event
+    * @see x-editable
+    */
+    public $onShown;
+    /**
+    * A javascript function that will be invoked when new value is saved
+    * Example:
+    * <pre>
+    * 'onSave' => 'js: function(e, params) {
+    *     alert("Saved value: " + params.newValue);
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package event
+    * @see x-editable
+    */
+    public $onSave;
+    /**
+    * A javascript function that will be invoked when editable form is hidden
+    * Example:
+    * <pre>
+    * 'onHidden' => 'js: function(e, reason) {
+    *    if(reason === "save" || reason === "cancel") {
+    *        //auto-open next editable
+    *        $(this).closest("tr").next().find(".editable").editable("show");
+    *    }
+    * }'
+    * </pre>
+    *
+    * @var string
+    * @package event
+    * @see x-editable
+    */
+    public $onHidden;
+
+    /**
+    * @var array all config options of x-editable. See full list <a href="http://vitalets.github.com/x-editable/docs.html#editable">here</a>.
+    */
+    public $options = array();
+
+    /**
+    * @var array HTML options of element
+    */
+    public $htmlOptions = array();
+
+    /**
+    * @var boolean whether to HTML encode text on output
+    */
+    public $encode = true;
+
+    /**
+    * @var boolean whether to apply 'editable' js plugin to element. 
+    * Only **safe** attributes become editable.
+    */
+    public $apply = null;
+
+    /**
+    * @var string title of popup. If `null` - will be generated automatically from attribute label.
+    * Can have token {label} inside that will be replaced with actual attribute label.
+    */
+    public $title = null;
+
+    //themeUrl, theme and cssFile copied from CJuiWidget to allow include custom theme for jQuery UI
+    /**
+     * @var string for jQuery UI only. The root URL that contains JUI theme folders.
+     * If not set, default Yii's theme will be used.
+    */
+    public $themeUrl;
+    /**
+     * @var string for jQuery UI only. The JUI theme name.
+     */
+    public $theme='base';
+    /**
+     * @var mixed for jQuery UI only. The theme CSS file name. By default Yii's jquery UI css used.
+    */
+    public $cssFile='jquery-ui.css';
+
+    private $_prepareToAutotext = false;
+    
+
+    /**
+    * initialization of widget
+    *
+    */
+    public function init()
+    {
+        parent::init();
+
+        if ($this->id === null) {
+          $this->id = uniqid();
+        }
+
+        $originalText = $this->text;
+
+        //if apply set to false --> just print text
+        if($this->apply === false) {
+            $this->text = $originalText;
+            return;
+        }
+
+        //if apply = false --> just print text (see 'run' method)
+        if ($this->apply === false) {
+            $this->text = $originalText;
+            return;
+        }
+
+        /*
+         try to detect type from metadata if not set
+        */
+        if ($this->type === null) {
+            $this->type = 'text';
+        }
+
+        /*
+        If set this flag to true --> element content will stay empty and value will be rendered to data-value attribute to apply autotext.
+        */
+        $this->_prepareToAutotext = (!isset($this->options['autotext']) || $this->options['autotext'] !== 'never') && in_array($this->type, array('select', 'checklist', 'date', 'dateui'));
+
+        /*
+         If text not defined, generate it from model attribute for types except lists ('select', 'checklist' etc)
+         For lists keep it empty to apply autotext
+        */
+        if (!strlen($this->text) && !$this->_prepareToAutotext) {
+            $this->text = $originalText;
+        }
+
+        $this->buildHtmlOptions();
+        $this->buildJsOptions();
+        $this->registerAssets();
+    }
+
+    public function buildHtmlOptions()
+    {
+        //html options
+        $htmlOptions = array(
+            'href'      => '#',
+            'rel'       => $this->getSelector(),
+            'data-pk'   => $this->id,
+        );
+
+        //if preparing to autotext we need to define value directly in data-value.
+        if ($this->_prepareToAutotext) {
+            //for date we use 'format' to put it into value (if text not defined)
+            if ($this->type == 'date') {
+                $this->value = $this->text;
+
+                //if date comes as object, format it to string
+                if($this->value instanceOf DateTime) {
+                    /*
+                    * unfortunatly datepicker's format does not match Yii locale dateFormat,
+                    * we need replacements below to convert date correctly
+                    */
+                    $count = 0;
+                    $format = str_replace('MM', 'MMMM', $this->format, $count);
+                    if(!$count) $format = str_replace('M', 'MMM', $format, $count);
+                    if(!$count) $format = str_replace('m', 'M', $format);
+
+                    $this->value = Yii::app()->dateFormatter->format($format, $this->value->getTimestamp());
+                }
+            } else {
+                $this->value = $this->text;
+            }
+
+            $this->htmlOptions['data-value'] = $this->value;
+        }
+
+        //merging options
+        $this->htmlOptions = CMap::mergeArray($this->htmlOptions, $htmlOptions);
+    }
+
+    public function buildJsOptions()
+    {
+        //normalize url from array
+        $this->url = CHtml::normalizeUrl($this->url);
+
+        //generate title from attribute label
+        if ($this->title === null) {
+            $titles = array(
+              'Select' => array('select', 'date'),
+              'Check' => array('checklist')
+            );
+            $title = Yii::t('EditableField.editable', 'Enter');
+            foreach($titles as $t => $types) {
+                if(in_array($this->type, $types)) {
+                   $title = Yii::t('EditableField.editable', $t);
+                }
+            }
+            $this->title = $title;
+        } else {
+            $this->title = strtr($this->title);
+        }
+
+        $options = array(
+            'type'  => $this->type,
+            'url'   => $this->url,
+            'name'  => $this->text,
+            'title' => CHtml::encode($this->title),
+        );
+
+        //options set directly in config
+        foreach(array('placement', 'emptytext', 'params', 'inputclass', 'format', 'viewformat') as $option) {
+            if ($this->$option) {
+                $options[$option] = $this->$option;
+            }
+        }
+
+        if ($this->source) {
+            //if source is array --> convert it to x-editable format.
+            //Note: source with count = 1 is Yii route
+            if(is_array($this->source) && count($this->source) > 1) {
+                //if first elem is array assume it's normal x-editable format, so just pass it
+                if(isset($this->source[0]) && is_array($this->source[0])) {
+                    $options['source'] = $this->source;
+                } else { //else convert to x-editable source format
+                    $options['source'] = array();
+                    foreach($this->source as $value => $text) {
+                        $options['source'][] = array('value' => $value, 'text' => $text);
+                    }
+                }
+            } else { //source is url
+                $options['source'] = CHtml::normalizeUrl($this->source);
+            }
+        }
+
+        //TODO: language for datepicker: use yii config's value if not defined directly
+
+        /*
+         unfortunatly datepicker's format does not match Yii locale dateFormat
+         so we cannot take format from application locale
+
+         see http://www.unicode.org/reports/tr35/#Date_Format_Patterns
+
+        if($this->type == 'date' && $this->format === null) {
+            $this->format = Yii::app()->locale->getDateFormat();
+        }
+        */
+        /*
+        if (isset($this->options['datepicker']) && !$this->options['datepicker']['language'] && yii::app()->language) {
+            $this->options['datepicker']['language'] = yii::app()->language;
+        }
+        */
+
+        //callbacks
+        foreach(array('validate', 'success', 'display') as $method) {
+            if(isset($this->$method)) {
+                $options[$method]=(strpos($this->$method, 'js:') !== 0 ? 'js:' : '') . $this->$method;
+            }
+        }
+
+        //merging options
+        $this->options = CMap::mergeArray($this->options, $options);
+    }
+
+    public function registerClientScript()
+    {
+        $script = "$('a[rel={$this->htmlOptions['rel']}]')";
+
+        //attach events
+        foreach(array('init', 'shown', 'save', 'hidden') as $event) {
+            $eventName = 'on'.ucfirst($event);
+            if (isset($this->$eventName)) {
+                // CJavaScriptExpression appeared only in 1.1.11, will turn to it later
+                //$event = ($this->onInit instanceof CJavaScriptExpression) ? $this->onInit : new CJavaScriptExpression($this->onInit);
+                $eventJs = (strpos($this->$eventName, 'js:') !== 0 ? 'js:' : '') . $this->$eventName;
+                $script .= "\n.on('".$event."', ".CJavaScript::encode($eventJs).")";
+            }
+        }
+
+        //apply editable
+        $options = CJavaScript::encode($this->options);
+        $script .= ".editable($options);";
+
+        Yii::app()->getClientScript()->registerScript(__CLASS__ . '#' . $this->id, $script);
+
+        return $script;
+    }
+
+     public function registerAssets()
+    {
+        $am = Yii::app()->getAssetManager();
+        $cs = Yii::app()->getClientScript();
+        $form = yii::app()->editable->form;
+        $mode = $this->mode ? $this->mode : yii::app()->editable->defaults['mode'];
+
+        // bootstrap
+        if($form === EditableConfig::FORM_BOOTSTRAP) {
+            if (($bootstrap = yii::app()->getComponent('bootstrap'))) {
+                $bootstrap->registerCoreCss();
+                $bootstrap->registerCoreScripts();
+            } else {
+                throw new CException('You need to setup Yii-bootstrap extension first.');
+            }
+
+            $assetsUrl = $am->publish(Yii::getPathOfAlias('editable.assets.bootstrap-editable'));
+            $js = 'bootstrap-editable.js';
+            $css = 'bootstrap-editable.css';
+        // jqueryui
+        } elseif($form === EditableConfig::FORM_JQUERYUI) {
+            if($mode === EditableConfig::POPUP && Yii::getVersion() < '1.1.13' ) {
+                throw new CException('jQuery UI editable popup supported from Yii 1.1.13+');
+            }
+
+            //register jquery ui
+            $this->registerJQueryUI();
+
+            $assetsUrl = $am->publish(Yii::getPathOfAlias('editable.assets.jqueryui-editable'));
+            $js = 'jqueryui-editable.js';
+            $css = 'jqueryui-editable.css';
+        // plain jQuery
+        } else {
+            $assetsUrl = $am->publish(Yii::getPathOfAlias('editable.assets.jquery-editable'));
+            $js = 'jquery-editable-poshytip.js';
+            $css = 'jquery-editable.css';
+
+            //publish & register poshytip for popup version
+            if($mode === EditableConfig::POPUP) {
+                $poshytipUrl = $am->publish(Yii::getPathOfAlias('editable.assets.poshytip'));
+                $cs->registerScriptFile($poshytipUrl . '/jquery.poshytip.js');
+                $cs->registerCssFile($poshytipUrl . '/tip-yellowsimple/tip-yellowsimple.css');
+            }
+
+            //register jquery ui for datepicker
+            if($this->type == 'date' || $this->type == 'dateui') {
+                $this->registerJQueryUI();
+            }
+        }
+
+        //register assets
+        $cs->registerCssFile($assetsUrl.'/css/'.$css);
+        $cs->registerScriptFile($assetsUrl.'/js/'.$js, CClientScript::POS_END);
+
+        //include moment.js if needed 
+        if($this->type == 'combodate') {
+            $momentUrl = $am->publish(Yii::getPathOfAlias('editable.assets.moment'));
+            $cs->registerScriptFile($momentUrl.'/moment.min.js');          
+        }
+        
+        //include select2 if needed
+        if($this->type == 'select2') {
+            $select2Url = $am->publish(Yii::getPathOfAlias('editable.assets.select2'));
+            $cs->registerScriptFile($select2Url.'/select2.min.js');  
+            $cs->registerCssFile($select2Url.'/select2.css');        
+        }        
+        
+        //TODO: include locale for datepicker
+        //may be do it manually?
+        /*
+        if ($this->type == 'date' && $this->language && substr($this->language, 0, 2) != 'en') {
+             //todo: check compare dp locale name with yii's
+             $localesUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('ext.editable.assets.js.locales'));
+             Yii::app()->clientScript->registerScriptFile($localesUrl . '/bootstrap-datepicker.'. str_replace('_', '-', $this->language).'.js', CClientScript::POS_END);
+        }
+        */
+    }
+
+    public function run()
+    {
+        if($this->apply !== false) {
+            $this->registerClientScript();
+            $this->renderLink();
+        } else {
+            $this->renderText();
+        }
+    }
+
+    public function renderLink()
+    {
+        echo CHtml::openTag('a', $this->htmlOptions);
+        $this->renderText();
+        echo CHtml::closeTag('a');
+    }
+
+    public function renderText()
+    {
+        $encodedText = $this->encode ? CHtml::encode($this->text) : $this->text;
+        if($this->type == 'textarea') {
+             $encodedText = preg_replace('/\r?\n/', '<br>', $encodedText);
+        }
+        echo $encodedText;
+    }
+
+    public function getSelector()
+    {
+        return $this->id;
+    }
+
+   /**
+    * method to register jQuery UI with build-in or custom theme
+    *
+    */
+    protected function registerJQueryUI()
+    {
+        $cs=Yii::app()->getClientScript();
+        if($this->themeUrl===null) {
+            $this->themeUrl=$cs->getCoreScriptUrl().'/jui/css';
+        }
+        $cs->registerCssFile($this->themeUrl.'/'.$this->theme.'/'.$this->cssFile);
+        $cs->registerPackage('jquery.ui');
+    }
+}
